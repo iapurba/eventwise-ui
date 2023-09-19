e5861a7bbfd22143222f11e7d96a59ed08718a6f
Apurba Panja <apurbapanja1@gmail.com>
2023-09-19 21:33:50 +0530

    Impl initial user profile page

---
+++ src/pages/UserProfile.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import UserProfileTabs from '../components/user/UserProfileTabs';
+
+const UserProfile = () => {
+  return (
+    <UserProfileTabs/>
+  );
+};
+
+export default UserProfile;
