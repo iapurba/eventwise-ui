aad9e781298e3a9f70dd847734ad3fdc67191af6
Apurba Panja <apurbapanja1@gmail.com>
2023-09-19 21:34:39 +0530

    Impl initial Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import RouteConfig from './routes/RouteConfig';
+import PrimaryHeader from './components/layouts/Header';
 
 const App = () => {
   return (
     <Provider store={store}>
+      <PrimaryHeader/>
       <RouteConfig />
     </Provider>
   );
