809ae45ce6c7032b60ee4bbefd0b20ac1bbd2a80
Apurba Panja <apurbapanja1@gmail.com>
2023-09-24 21:05:03 +0530

    Implement Edit profile forms ui

---
+++ src/components/forms/ContactDetailsForm.tsx
@@ -0,0 +1,54 @@
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import FormHeader from '../common/FormHeader';
+import StyledTextField from '../common/StyledTextField';
+
+
+const ContactDetailsForm = () => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            <FormHeader title={'Please update your account details below'} />
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <StyledTextField
+                            fullWidth
+                            id="firstName"
+                            name="firstName"
+                            type="text"
+                            placeholder="First Name"
+                            size="small"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <StyledTextField
+                            fullWidth
+                            id="lastName"
+                            name="lastName"
+                            type="text"
+                            placeholder="Last Name"
+                            size="small"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <StyledTextField
+                            fullWidth
+                            id="phone"
+                            name="phone"
+                            type="text"
+                            placeholder="Phone Number"
+                            size="small"
+                        />
+                    </Grid>
+                </Grid>
+            </Box>
+        </Box>
+    );
+}
+
+export default ContactDetailsForm;
