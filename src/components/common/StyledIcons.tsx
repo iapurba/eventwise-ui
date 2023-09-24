64b8a9a7367bc7ca09929ae1db9abb06bd5a72a5
Apurba Panja <apurbapanja1@gmail.com>
2023-09-24 17:28:18 +0530

    Modify icons names

--- src/components/common/StyledIcons.tsx
+++ src/components/common/StyledIcons.tsx
@@ -1,46 +1,54 @@
-import styled from '@emotion/styled';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import EventOutlinedIcon from '@mui/icons-material/EventOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import TrendingUpOutlinedIcon from '@mui/icons-material/TrendingUpOutlined';
+import { styled } from '@mui/material/styles';
 
-export const CustomLocationIcon = styled(LocationOnOutlinedIcon)(({ theme }) => ({
+export const StyledLocationIcon = styled(LocationOnOutlinedIcon)(() => ({
     fontSize: '20px',
     color: '#45474D',
     '&.MuiSvgIcon-root': {
         margin: 0,
         padding: 0,
-        strokeWidth: '1px',
     },
 }));
 
-export const CustomEventIcon = styled(EventOutlinedIcon)(({ theme }) => ({
+export const StyledEventIcon = styled(EventOutlinedIcon)(() => ({
     fontSize: '20px',
     color: '#45474D',
     '&.MuiSvgIcon-root': {
         margin: 0,
         padding: 0,
-        strokeWidth: '1px',
     },
 }));
 
-export const CustomBookmarkIcon = styled(BookmarkBorderIcon)(({ theme }) => ({
+export const StyledBookmarkIcon = styled(BookmarkBorderIcon)(() => ({
     fontSize: '20px',
     color: '#45474D',
     '&.MuiSvgIcon-root': {
         margin: 0,
         padding: 0,
-        strokeWidth: '1px',
     },
 }));
 
-export const CustomFavouriteIcon = styled(FavoriteBorderOutlinedIcon)(({theme}) =>({
+export const StyledFavouriteIcon = styled(FavoriteBorderOutlinedIcon)(() => ({
     fontSize: '20px',
     color: '#45474D',
     '&.MuiSvgIcon-root': {
         margin: 0,
         padding: 0,
-        strokeWidth: '1px',
     },
 }));
 
+export const StyledTrendingIcon = styled(TrendingUpOutlinedIcon)(() => ({
+    fontSize: '18px',
+    fontWeight: 'bold',
+    background: '#EC1066',
+    borderRadius: '20px',
+    color: '#FFFFFF',
+    '&.MuiSvgIcon-root': {
+        margin: 0,
+        padding: '2px',
+    },
+}));
