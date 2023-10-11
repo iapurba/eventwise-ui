95f9ef92bb81631e9869bb0fb264ce20eca61124
Apurba Panja <apurbapanja1@gmail.com>
2023-10-12 01:51:38 +0530

    add mock data

---
+++ src/mock/events.ts
@@ -0,0 +1,106 @@
+export const events = [
+    {
+      _id: "64f71d4b273a6dbe15114cea",
+      title: 'When Chai Met Toast - Love You The Same Tour Some More words here | Kolkata - Long text which is more than two lines',
+      description: `Get ready for a soul-stirring musical journey as the enchanting band, When Chai Met Toast, embarks on an extraordinary India tour spanning 8 vibrant cities. The atmosphere is set to be nothing short of magical, filled with the mellifluous melodies of "So Beautiful, with You" and other chart-topping hits. As they take the stage, the crowd is greeted with the warmth of a thousand suns, their spirits lifted by the familiar refrain of "Khoj (Passing By)" and "Firefly." The band's euphoric blend of folk-pop will transport you to a whimsical world, reminiscent of the lyrics from "Love You The Same" as they embrace the mantra of hope and resilience. `,
+      category: 'music',
+      eventType: 'physical',
+      location: {
+        venue: 'JW Marriott Hotel',
+        address: {
+          street: '4A, JBS Haldane Ave, Tangra',
+          city: 'Kolkata',
+          state: 'West Bengal',
+          pincode: '700105'
+        }
+      },
+      date: "2023-12-02",
+      startTime: '7 PM',
+      duration: '3 hrs',
+      capacity: 2000,
+      artists: [],
+      tags: [],
+      createdBy: "64f6c02295004c5b087bc758",
+      performances: [],
+      createdAt: "2023-09-05T12:21:31.249Z",
+      updatedAt: "2023-09-05T12:21:31.249Z",
+      __v: 0
+    },
+    {
+      _id: "64f746627f95cd45e4261f43",
+      title: 'Beyond Fusion',
+      description: 'We are thrilled to present a mesmerizing music concert that brings together some of the most extraordinary and illustrious artists in the realm of Indian classical music. Prepare to be captivated by the enchanting sounds of PT.Tejendra Narayan Majumder, Shivmani, PT.Bickram Ghosh, and Kaushiki Chakraborty as they take the stage for a night of pure musical magic.',
+      category: 'music',
+      eventType: 'physical',
+      location: {
+        venue: 'Nazrul Mancha',
+        address: {
+          street: 'G977+728 Southern Park Southern Avenue, near Rabindra Sarobar, Lake, Dhakuria, Rabindra Sarobar',
+          city: 'Kolkata',
+          state: 'West Bengal',
+          pincode: '700020'
+        }
+      },
+      date: '2023-11-11',
+      startTime: '8 PM',
+      duration: '4 hrs',
+      capacity: 5000,
+      artists: [
+        'PT.Tejendra Narayan Majumder',
+        'Shivmani',
+        'PT.Bickram Ghosh',
+        'Kaushiki Chakraborty'
+      ],
+      tags: [ '#music', '#kolkata' ],
+      createdBy: "64f6c577d70f5296285370ad",
+      performances: []
+    },
+    {
+      _id: "64f746627f95cd45e4261f44",
+      title: 'Anupraas Alankaar by Aditya Jain',
+      description: '',
+      category: 'comedy',
+      eventType: 'physical',
+      location: {
+        venue: 'Ministry Of Comedy',
+        address: {
+          street: '18th Main Road, 138/B, 5th Cross Rd, behind Cafe Coffee Day, KHB Colony, 5th Block, Koramangala',
+          city: 'Bengaluru',
+          state: 'Karnataka',
+          pincode: '560034'
+        }
+      },
+      date: '2023-12-08',
+      startTime: '6:30 PM',
+      duration: '4 hrs',
+      capacity: 2000,
+      artists: [ 'Aditya Jain' ],
+      tags: [ '#comedy', '#bengaluru', '#AdityaJain' ],
+      createdBy: "64f6c577d70f5296285370ad",
+      performances: []
+    },
+    {
+      _id: "64f746627f95cd45e4261f45",
+      title: 'Travel Writing Masterclassr',
+      description: '',
+      category: 'workshop',
+      eventType: 'physical',
+      location: {
+        venue: 'Atta Galata',
+        address: {
+          street: '178, 5th Main Rd, Binnamangala, Hoysala Nagar, Indiranagar',
+          city: 'Bengaluru',
+          state: 'Karnataka',
+          pincode: '560038'
+        }
+      },
+      date: '2024-02-25',
+      startTime: '5 PM',
+      duration: '3 hrs',
+      capacity: 500,
+      artists: [],
+      tags: [ '#workshop', '#bengaluru', '#masterclass', '#writting' ],
+      createdBy: "64f6c577d70f5296285370ad",
+      performances: []
+    }
+  ]
