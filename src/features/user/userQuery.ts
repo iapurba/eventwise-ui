70ef44e9d5b69423a737f8e19f187f55428df540
Apurba Panja <apurbapanja1@gmail.com>
2024-01-21 16:28:56 +0530

    Deleted files to restrucutre project

--- src/features/user/userQuery.ts
+++
@@ -1,6 +0,0 @@
-import { useGetUserProfileQuery } from './userApi';
-
-export const useGetUserProfile = () => {
-    const { data: userProfile, error, isLoading } = useGetUserProfileQuery();
-    return { userProfile, error, isLoading };
-};
